62ea04fb980a66250f6aa6132de76cf005f9fa90
Dankestmemes1 <77721747+Dankestmemes1@users.noreply.github.com>
2021-07-12 19:00:50 -0700

    Update Windows%X.lua

--- Windows%X.lua
+++ Windows%X.lua
@@ -1,5 +1,7 @@
+local Notification = loadstring(game:HttpGet("https://api.irisapp.ca/Scripts/IrisBetterNotifications.lua"))()
+
 Notification.Notify("patched", "patched dont use", "", {
-Duration = 2,       
+Duration = 100,       
 Main = {
     Rounding = true,
 }
